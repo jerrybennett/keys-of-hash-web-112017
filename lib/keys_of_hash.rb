a7b50746e0c69aa636704107c83b403186e30d28
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-14 19:00:10 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguments)
     items = []
     for item in arguments
-      each do |key, value|
+      item.each do |key, value|
         if item == value
           items << key
         end
